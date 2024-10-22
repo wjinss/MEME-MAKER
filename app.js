6ce5542554aec207f0bc845e7e6efd78cd750eeb
wjinss <aastu4343@gmail.com>
2024-10-22 15:07:28 +0900

    1.4

--- app.js
+++ app.js
@@ -2,28 +2,25 @@ const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 canvas.width = 800
 canvas.height = 800
-
 ctx.lineWidth = 1
-const colors =[
-"#ff3838",
-"#ffb8b8",
-"#c56cf0",
-"#ff9f1a",
-"#fff200",
-"#32ff7e",
-"#7efff5",
-"#18dcff",
-"#7d5fff",
-]
 
-function onClick(e){
-    ctx.beginPath()
-    
-    ctx.moveTo(Math.random()*800,Math.random()*800)
-    const color = colors[Math.floor(Math.random() * colors.length)]
-    ctx.strokeStyle = color
-    ctx.lineTo(e.offsetX, e.offsetY)
-    ctx.stroke()
-    
+let isPainting = false
+function onMove(e){
+    if(isPainting){
+        ctx.lineTo(e.offsetX, e.offsetY)
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(e.offsetX, e.offsetY)
 }
-canvas.addEventListener("mousemove", onClick)
+function startPainting(){
+    isPainting = true
+}
+function canclePainting(){
+    isPainting = false
+}
+canvas.addEventListener("mousemove", onMove)
+canvas.addEventListener("mousedown", startPainting)
+canvas.addEventListener("mouseup", canclePainting)
+canvas.addEventListener("mouseleave", canclePainting)
+
