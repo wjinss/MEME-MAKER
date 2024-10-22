305e67abea359947784da09f93d353fa58f3469a
wjinss <aastu4343@gmail.com>
2024-10-22 13:44:15 +0900

    1.2

--- app.js
+++ app.js
@@ -3,26 +3,16 @@ const ctx = canvas.getContext("2d")
 canvas.width = 800
 canvas.height = 800
 
+ctx.fillRect(350 - 40,200 - 30,15,100)
+ctx.fillRect(260 - 40,200 - 30,60,200)
+ctx.fillRect(220 - 40,200 - 30,15,100)
 
-// ctx.rect(50,50,100,100)
-// ctx.rect(150,150,100,100)
-// ctx.rect(250,250,100,100)
-// ctx.moveTo(50, 50) //좌표를 0,0(디폴트)에서 50,50으로 옮김
-// ctx.lineTo(150,50) //x150,y50의 좌표로 선을 긋는다
-// ctx.lineTo(150,150)
-// ctx.lineTo(50,150)
-// ctx.lineTo(50,50)
-// ctx.fill() 
+ctx.arc(250, 100, 50, 0, 2 * Math.PI) //arc에서 endangle값을 2로 화면 정원이 그려짐
+ctx.fill()
 
-// ctx.beginPath()
-// ctx.rect(350,350,100,100)
-// ctx.fillStyle="red"
-// ctx.fill() 
-ctx.fillRect(200,200,50,200)
-ctx.fillRect(400,200,50,200)
-ctx.fillRect(300,300,50,100)
-ctx.fillRect(200,200,200,20)
-ctx.moveTo(200,200)
-ctx.lineTo(325,100)
-ctx.lineTo(450,200)
+
+ctx.beginPath()//색상을 변경할려면 기존의 패스를 끊고 새로 시작해야됨.
+ctx.fillStyle = "cyan"
+ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI)
+ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI)
 ctx.fill()
