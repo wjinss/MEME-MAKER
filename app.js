f5b7d459771614da52e98bafd0bc899f70396344
wjinss <aastu4343@gmail.com>
2024-10-22 16:31:53 +0900

    1.8

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const btnMode = document.querySelector("#btn_mode")
 const colorOptions = Array.from(document.querySelectorAll(".color_option"))
 const color = document.querySelector("#color")
 const lineWidth = document.querySelector("#line_width")
@@ -8,6 +9,8 @@ canvas.height = 800
 ctx.lineWidth = lineWidth.value
 
 let isPainting = false
+let isFilling = false
+
 function onMove(e){
     if(isPainting){
         ctx.lineTo(e.offsetX, e.offsetY)
@@ -36,12 +39,28 @@ function onColorClick(e){
     // ctx.fillStyle = colorValue
     // color.value = colorValue
 }
+function onModeClick(){
+    if(isFilling){
+        isFilling = false   
+        btnMode.innerText = "Fill"
+    } else{
+        isFilling = true
+        btnMode.innerText = "Draw"
+    }
+}
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0,0,800,800)
+    }
+}
 
 canvas.addEventListener("mousemove", onMove)
 canvas.addEventListener("mousedown", startPainting)
 canvas.addEventListener("mouseup", canclePainting)
 canvas.addEventListener("mouseleave", canclePainting)
-
+canvas.addEventListener("click", onCanvasClick)
 lineWidth.addEventListener("change", onLineWidthChange)
 color.addEventListener("change", onColorChange)
 colorOptions.forEach(color => color.addEventListener("click", onColorClick))
+
+btnMode.addEventListener("click", onModeClick)
